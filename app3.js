aad173f8caa96a3ed45f13b0930a5e1896d2018f
Patryk Szczygło <patysprog@gmail.com>
2017-07-03 12:42:24 +0200

    filtered using ilter

--- app3.js
+++ app3.js
@@ -10,3 +10,20 @@ var doubledMapped = data.map(function(item) {
 });
 
 console.log(doubledMapped);
+
+//////////
+
+var data2 = [1,2,3,3,4,4,5,6,7,8];
+
+var evens = data2.reduce(function(acc, value) {
+  if(value%2 === 0) {
+    acc.push(value);
+  }
+  return acc;
+}, []);
+
+var evensFiltered = data2.filter(function(item) {
+  return (item%2 === 0);
+});
+
+console.log(evensFiltered);
