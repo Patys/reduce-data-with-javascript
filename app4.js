75d8410988297996a80d1dfa823bb9d3d3dbd810
Patryk Szczygło <patysprog@gmail.com>
2017-07-03 13:06:11 +0200

    avarge computed by reducer

--- app4.js
+++ app4.js
@@ -1,8 +1,12 @@
-function reducer(acc, val) {
-  return acc + val;
+function reducer(acc, val, index, array) {
+  let intermediaryValue = acc + val;
+  if(index === array.length-1) {
+    return intermediaryValue / array.length;
+  }
+  return intermediaryValue;
 }
 
 var data = [1,2,3,4,5,56,6];
-var sum = data.reduce(reducer,0);
+var avarge = data.reduce(reducer,0);
 
-console.log(sum / data.length);
+console.log(avarge);
