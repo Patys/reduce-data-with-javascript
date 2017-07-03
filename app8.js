f33b061617695cca209d60650ca803219bc86e1d
Patryk Szczygło <patysprog@gmail.com>
2017-07-03 13:44:04 +0200

    lesson8 (data copied from lesson)

---
+++ app8.js
@@ -0,0 +1,37 @@
+var luke = {
+  name: "luke skywalker",
+  jedi: true,
+  parents: {
+    father: {
+      jedi: true
+    },
+    mother: {
+      jedi: false
+    }
+  }
+}
+
+var han = {
+  name: "han solo",
+  jedi: false,
+  parents: {
+    father: {
+      jedi: false
+    },
+    mother: {
+      jedi: false
+    }
+  }
+}
+
+var anakin = {
+  name: "anakin skywalker",
+  jedi: true,
+  parents: {
+    mother: {
+      jedi: false
+    }
+  }
+}
+
+var characters = [luke, han, anakin];
