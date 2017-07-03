9355e655fbb55fb40c269c1b78cfe8514b7b53a8
Patryk Szczygło <patysprog@gmail.com>
2017-07-03 13:38:47 +0200

    added functions

---
+++ app7.js
@@ -0,0 +1,4 @@
+function incement(input) { return input + 1; }
+function decrement(input) { return input - 1; }
+function double(input) { return input * 2; }
+function halve(input) { return input / 2; }
