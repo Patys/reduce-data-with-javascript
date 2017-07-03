753cd0d616547275aba2cbb76def840f0154ac4b
Patryk Szczygło <patysprog@gmail.com>
2017-07-03 13:42:19 +0200

    using pipeline

--- app7.js
+++ app7.js
@@ -1,4 +1,22 @@
-function incement(input) { return input + 1; }
+function increment(input) { return input + 1; }
 function decrement(input) { return input - 1; }
 function double(input) { return input * 2; }
 function halve(input) { return input / 2; }
+
+let initialValue = 1;
+
+var pipeline = [
+  increment,
+  increment,
+  double,
+  decrement,
+  halve,
+  decrement,
+  double
+]
+
+var finalValue = pipeline.reduce((acc, fn) => {
+  return fn(acc);
+}, initialValue);
+
+console.log(finalValue);
