b181cfdc9853f9d9b8f484f945cd37f5bb946848
Patryk Szczygło <patysprog@gmail.com>
2017-07-03 12:30:37 +0200

    added reducer

--- app2.js
+++ app2.js
@@ -6,4 +6,20 @@ var votes = [
   "react",
   "angular",
   "react"
-]
+];
+
+var initialValue = {}
+
+var reducer = function(tally, vote) {
+  if(!tally[vote]) {
+    tally[vote] = 1;
+  } else {
+    tally[vote] += 1;
+  }
+
+  return tally;
+};
+
+var result = votes.reduce(reducer, initialValue);
+
+console.log(result);
